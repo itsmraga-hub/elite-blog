8b1b0414a1e0c02d041d4f636a51c5f1c0e378d8
Raga <ragawilliam570@gmail.com>
2022-11-22 16:34:16 +0300

    Add Post id column to Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_132905) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_133143) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_132905) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "users"
+    t.integer "posts"
+    t.index ["posts"], name: "index_comments_on_posts"
     t.index ["users"], name: "index_comments_on_users"
   end
 
