7ec00adebc4aba3a5ef3638b1ba303246a2586f2
Raga <ragawilliam570@gmail.com>
2022-11-24 13:12:54 +0300

    Add validation for name is User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
 
+  # Validations
+  validates :name, presence: true
+   
+
   def latest_three
     posts.limit(3).order(created_at: :desc)
   end
