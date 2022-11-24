a58a0b6942ab48de8b36fac0da65527bfed16c20
Raga <ragawilliam570@gmail.com>
2022-11-24 13:21:56 +0300

    Add validation for postsCounter integer and >= 0

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   # Validations
   validates :name, presence: true
-   
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0}
 
   def latest_three
     posts.limit(3).order(created_at: :desc)
