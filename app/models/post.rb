a71976867670ebbca721ca336e604634585ddcbe
Raga <ragawilliam570@gmail.com>
2022-11-24 13:27:17 +0300

    Add validation for likes_counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,7 @@ class Post < ApplicationRecord
   # Validations
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0}
 
   # Update by use author associated with post
   def posts_number_update
