0a1dadee5497dff5a0907e6a2b5caa70fad5424e
Raga <ragawilliam570@gmail.com>
2022-11-25 12:32:05 +0300

    Add unit test for initialize user variables

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # tests go here
+  subject { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+  
+  before { subject.save }
+
+  it 'Should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Photo should be present' do
+    subject.photo = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Bio should be present' do
+    subject.bio = nil
+    expect(subject).to_not be_valid
+  end
+
+  it '' do
+
+  end
+end
