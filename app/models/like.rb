f27a8ad4e9eff4229de54f884c6939554628e449
Raga <ragawilliam570@gmail.com>
2022-11-25 14:29:04 +0300

    Add autoincrement methods on save

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User'
 
+  after_save :likes_counter_update
+
   def likes_counter_update
     post.update(likes_counter: post.likes.all.length)
   end
