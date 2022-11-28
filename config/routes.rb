0bb3189063073dc007a661d83f780bc5b1ef40b0
Raga <ragawilliam570@gmail.com>
2022-11-28 17:10:22 +0300

    Make changes to improve readability

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  # get 'users/index'
-  get 'users/', to: "users#index", as: 'users'
-  get 'users/:id', to: "users#show", as: 'user'
-  get 'users/:user_id/posts', to: "posts#index", as: 'user_posts'
-  get 'users/:user_id/posts/:id', to: "posts#show", as: 'user_post'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end 
+  # # get 'users/index'
+  # get 'users/', to: "users#index", as: 'users'
+  # get 'users/:id', to: "users#show", as: 'user'
+  # get 'users/:user_id/posts', to: "posts#index", as: 'user_posts'
+  # get 'users/:user_id/posts/:id', to: "posts#show", as: 'user_post'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
