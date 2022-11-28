9ba068dfb405cb0a2bf9bc2f29afe9e9d4982ab1
Raga <ragawilliam570@gmail.com>
2022-11-28 16:46:29 +0300

    Add route to index for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  # get 'users/index'
+  get 'users/', to: "users#index", as: 'users'
+  # get 'users/:id', to: "users#show", as: 'user'
+  # get 'users/:user_id/posts', to: "posts#index", as: 'user_posts'
+  # get 'users/:user_id/posts/:id', to: "posts#show", as: 'user_post'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "users#index"
 end
