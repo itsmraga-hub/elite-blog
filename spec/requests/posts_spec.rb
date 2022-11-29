bc9d3d217180572a3e7f25abe9d0c30f1de3ddf4
Raga <ragawilliam570@gmail.com>
2022-11-29 09:02:27 +0300

    Add tests for users/:user_id/posts/:post_id requests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -15,4 +15,19 @@ RSpec.describe 'Posts', type: :request do
     get '/users/1/posts/'
     expect(response.body).to include("Posts")
   end
+
+  it 'returns http success' do
+    get '/users/1/posts/1'
+    expect(response).to have_http_status(:success)
+  end
+
+  it 'to have template show for one user' do
+    get '/users/1/posts/1'
+    expect(response).to render_template(:show)
+  end
+
+  it 'Body incudes correct placeholder text' do
+    get '/users/1/posts/1'
+    expect(response.body).to include("Post 1")
+  end
 end
