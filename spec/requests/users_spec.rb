a78e1307ecb5d51785a151731e2c5b2b77aff9de
Raga <ragawilliam570@gmail.com>
2022-11-29 09:00:39 +0300

    Add tests for users/ request

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/users/index'
-      expect(response).to have_http_status(:success)
-    end
+  it 'returns http success' do
+    get '/users/'
+    expect(response).to have_http_status(:success)
+  end
+
+  it 'to have template index for all users' do
+    get '/users/'
+    expect(response).to render_template(:index)
+  end
+
+  it 'Body incudes correct placeholder text: Users' do
+    get '/users/'
+    expect(response.body).to include("Users")
   end
 end
