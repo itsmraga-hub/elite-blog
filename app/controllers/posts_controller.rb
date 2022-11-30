0d4ed2543cf4a8ab324e68b915d555d61eae3ec3
Raga <ragawilliam570@gmail.com>
2022-11-30 23:39:26 +0300

    Add index methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,11 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @user_posts = @user.posts
+    # @user_posts = @user.posts
   end
 
   def show
     @user = User.find(params[:user_id])
-    @post = @user.posts.find_by_id(params[:id])
-    @post_comments = @post.comments
+    @post = Post.find(params[:id])
   end
 end
