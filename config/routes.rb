eb35bac5f1524548fb43d3697fc856d35a8482a6
Raga <ragawilliam570@gmail.com>
2022-12-01 17:24:59 +0300

    Add likes create method

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new]
-  end 
-  # # get 'users/index'
-  # get 'users/', to: "users#index", as: 'users'
-  # get 'users/:id', to: "users#show", as: 'user'
-  # get 'users/:user_id/posts', to: "posts#index", as: 'user_posts'
-  # get 'users/:user_id/posts/:id', to: "posts#show", as: 'user_post'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  end
+  post '/users/:user_id/posts/new', to: "posts#create", as: 'create_post'
+  post '/users/:user_id/posts/:id/likes', to "likes#create", as: 'create_like'
 
   # Defines the root path route ("/")
-  # root "users#index"
+  root "users#index"
 end
