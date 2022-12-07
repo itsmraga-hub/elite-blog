79776639bf8d80bf4bb35e80766d850016c72729
Raga <ragawilliam570@gmail.com>
2022-12-07 11:37:10 +0300

    Fix n+1 problems for comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,12 @@
 class PostsController < ApplicationController
   def index
-    @user = User.includes(:posts, :comments).find(params[:user_id])
+    @user = User.find(params[:user_id])
   end
 
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
+    @comments = @post.comments.includes([:author])
   end
 
   def new
