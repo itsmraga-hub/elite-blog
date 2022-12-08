d91a9c2e152d1c68adf0dd401747cda78deb2503
Raga <ragawilliam570@gmail.com>
2022-12-08 16:15:20 +0300

    Add is? method to user for authoriztion

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
+
+  Roles = [:admin, :default]
+
+  def is?(requested_role)
+    self.role == requested_role.to_s
+  end
 
   # Validations
   validates :name, presence: true
