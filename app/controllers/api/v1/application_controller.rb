cf5d262e36f8071eeb9ce9fc8f2f81aa48676d3b
Raga <ragawilliam570@gmail.com>
2022-12-09 23:05:57 +0300

    Add api application controller

---
+++ app/controllers/api/v1/application_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::ApplicationController < ActionController::API
+  # protect_from_forgery with: :exception
+  before_action :authentication_with_token
+  before_action :authenticate_user!
+
+  private
+  
+  def authentication_with_token
+    if params[:api_token]
+      user = User.find_by_api_token(params[:api_token])
+      sign_in(user)
+    end
+  end
+end
