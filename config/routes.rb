ddff23220b65c8605e14bc7e0658707b39ab62d5
Raga <ragawilliam570@gmail.com>
2022-12-10 13:10:44 +0300

    Make reviewwer changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get 'likes/create'
+
   devise_scope :user do
     root to: 'devise/sessions#new'
   end
+  
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments, only: [:new, :create, :destroy]
@@ -16,10 +17,10 @@ Rails.application.routes.draw do
       get 'api_token'
     end
   end
+
   # API routes
   namespace :api do
     namespace :v1 do
-      # post 'auth/login', to 'authenticate#login'
       resources :users do 
         resources :posts do 
           resources :comments
